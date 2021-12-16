32f9e66b913dd8a62e6716779d17f6614cd8e852
Dr.MoPo3ilo <1853560+mopo3ilo@users.noreply.github.com>
2021-12-16 07:34:55 +0300

    Новые объекты

--- consts.go
+++ consts.go
@@ -1,7 +1,7 @@
 package sql1cv8
 
 // Типы полей
-var consts = map[string]string{
+var types = map[string]string{
 	"Тип.NULL":         "NULL",
 	"Тип.Неопределено": "0x01",
 	"Тип.Булево":       "0x02",
@@ -12,40 +12,39 @@ var consts = map[string]string{
 	"Тип.Ссылка":       "0x08",
 }
 
-func initConsts(m Metadata) {
-	for k, v := range consts {
-		m.Tables[k] = &Object{
-			DBName: v,
-			CVName: k,
-		}
-	}
-}
-
 // Стандартные поля
 var fields = map[string]string{
-	"_Active":       "Активность",
-	"_Code":         "Код",
-	"_Date_Time":    "Дата",
-	"_Description":  "Наименование",
-	"_DimHash":      "ХэшИзмерений",
-	"_EnumOrder":    "Порядок",
-	"_Folder":       "Группа",
-	"_IDRRef":       "Ссылка",
-	"_KeyField":     "КлючЗаписи",
-	"_LineNo":       "НомерСтроки",
-	"_Marked":       "ПометкаУдаления",
-	"_Number":       "Номер",
-	"_NumberPrefix": "Префикс",
-	"_OwnerID":      "Владелец",
-	"_ParentIDRRef": "Родитель",
-	"_Period":       "Период",
-	"_Posted":       "Проведен",
-	"_PredefinedID": "Предопределенное",
-	"_RecorderRRef": "Регистратор",
-	"_RecorderTRef": "ВидРегистратора",
-	"_RecordKey":    "КлючЗаписи",
-	"_RecordKind":   "ВидДвижения",
-	"_Splitter":     "Разделитель",
-	"_Type":         "ТипЗначения",
-	"_Version":      "Версия",
+	"_Active":          "Активность",
+	"_BusinessProcess": "БизнесПроцесс",
+	"_Code":            "Код",
+	"_Completed":       "Завершен",
+	"_Date_Time":       "Дата",
+	"_Description":     "Наименование",
+	"_DimHash":         "ХэшИзмерений",
+	"_EnumOrder":       "Порядок",
+	"_Executed":        "Выполнена",
+	"_Folder":          "Группа",
+	"_HeadTaskRRef":    "ВедущаяЗадача",
+	"_IDRRef":          "Ссылка",
+	"_KeyField":        "КлючЗаписи",
+	"_LineNo":          "НомерСтроки",
+	"_Marked":          "ПометкаУдаления",
+	"_Name":            "Наименование",
+	"_Number":          "Номер",
+	"_NumberPrefix":    "Префикс",
+	"_OwnerID":         "Владелец",
+	"_ParentIDRRef":    "Родитель",
+	"_Period":          "Период",
+	"_Point":           "ТочкаМаршрута",
+	"_Posted":          "Проведен",
+	"_PredefinedID":    "Предопределенное",
+	"_RecorderRRef":    "Регистратор",
+	"_RecorderTRef":    "ВидРегистратора",
+	"_RecordKey":       "КлючЗаписи",
+	"_RecordKind":      "ВидДвижения",
+	"_RoutePointOrder": "Порядок",
+	"_Splitter":        "Разделитель",
+	"_Started":         "Стартован",
+	"_Type":            "ТипЗначения",
+	"_Version":         "Версия",
 }
--- metadata.go
+++ metadata.go
@@ -1,16 +1,20 @@
 package sql1cv8
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 	"strconv"
 )
 
 // Object объект метаданных
 type Object struct {
-	Number string             // Номер объекта в десятеричной системе
-	DBName string             // Имя объекта в базе данных
-	CVName string             // Имя объекта в конфигурации
-	Fields map[string]*Object // Параметры объекта
+	UUID     string             // Идентификатор
+	Number   string             // Номер объекта DBNames
+	DBName   string             // Имя в базе данных
+	CVName   string             // Имя в конфигурации
+	Synonyms map[string]string  // Синонимы объекта
+	Params   map[string]*Object // Параметры объекта
 }
 
 // RTRefInt возвращает ВидСсылки типа INT.
@@ -34,5 +38,25 @@ func (o *Object) RTRefBin() (string, error) {
 // Metadata метаданные
 type Metadata struct {
 	Version string             // Версия метаданных
-	Tables  map[string]*Object // Объекты метаданных первого уровня. Это либо таблицы, либо какие-то констаты вроде типов полей для составных типов, значения перечислений и виды ссылок
+	Objects map[string]*Object // Объекты метаданных первого уровня. Это либо таблицы, либо какие-то констаты вроде типов полей для составных типов, значения перечислений и виды ссылок
+}
+
+// SaveToFile сохраняет метаданные в файл.
+// В качестве параметров принимает строковую переменную:
+// s - имя файла, в котором хранится кэш метаданных в формате json.
+func (m Metadata) SaveToFile(s string) (err error) {
+	b, err := json.Marshal(m)
+	if err != nil {
+		return
+	}
+	f, err := os.Create(s)
+	if err != nil {
+		return
+	}
+	defer f.Close()
+	_, err = f.Write(b)
+	if err != nil {
+		return
+	}
+	return
 }
